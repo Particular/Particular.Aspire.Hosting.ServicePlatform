f20a520153eb49ef409b538a62f38f1258ac018a
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-05-18 14:33:48 +0800

    remove null checks

--- src/Particular.Aspire.Hosting.ServicePlatform/ThroughputReporting/ThroughputReportingAmazonSqs.cs
+++ src/Particular.Aspire.Hosting.ServicePlatform/ThroughputReporting/ThroughputReportingAmazonSqs.cs
@@ -10,37 +10,19 @@ using Transport;
 /// This is used to configure the usage reporting component in Service Control as described
 /// at https://docs.particular.net/servicecontrol/servicecontrol-instances/configuration?version=servicecontrol_4#usage-reporting-when-using-the-amazon-sqs-transport
 /// </summary>
-public sealed class ThroughputReportingAmazonSqs : IThroughputReportingProvider
+public sealed class ThroughputReportingAmazonSqs(
+    ReferenceExpression? accessKeyId = null,
+    ReferenceExpression? secretKey = null,
+    ReferenceExpression? region = null,
+    ReferenceExpression? profile = null,
+    ReferenceExpression? prefix = null)
+    : IThroughputReportingProvider
 {
     internal const string AccessKeyEnvVar = "LICENSINGCOMPONENT_AMAZONSQS_ACCESSKEY";
     internal const string SecretAccessKeyEnvVar = "LICENSINGCOMPONENT_AMAZONSQS_SECRETKEY";
     internal const string ProfileEnvVar = "LICENSINGCOMPONENT_AMAZONSQS_PROFILE";
     internal const string RegionEnvVar = "LICENSINGCOMPONENT_AMAZONSQS_REGION";
     internal const string QueueNamePrefixEnvVar = "LICENSINGCOMPONENT_AMAZONSQS_PREFIX";
-
-    readonly ReferenceExpression? accessKeyId;
-    readonly ReferenceExpression? secretKey;
-    readonly ReferenceExpression? profile;
-    readonly ReferenceExpression? region;
-    readonly ReferenceExpression? prefix;
-
-    public ThroughputReportingAmazonSqs(
-        ReferenceExpression? accessKeyId = null,
-        ReferenceExpression? secretKey = null,
-        ReferenceExpression? region = null,
-        ReferenceExpression? profile = null,
-        ReferenceExpression? prefix = null)
-    {
-        ArgumentNullException.ThrowIfNull(accessKeyId);
-        ArgumentNullException.ThrowIfNull(secretKey);
-        ArgumentNullException.ThrowIfNull(region);
-
-        this.accessKeyId = accessKeyId;
-        this.secretKey = secretKey;
-        this.region = region;
-        this.profile = profile;
-        this.prefix = prefix;
-    }
 
     public void ApplyTo(IResourceBuilder<ServiceControlErrorInstanceResource> errorInstance)
     {
