a67d33475047757ff082839740e2afa53dc59fb4
Rhys Bevilaqua <rhys.bevilaqua@particular.net>
2026-05-18 14:51:07 +0800

    make type nullable

--- src/Particular.Aspire.Hosting.ServicePlatform.UnitTests/AspireApplicationPublishingTestBase.cs
+++ src/Particular.Aspire.Hosting.ServicePlatform.UnitTests/AspireApplicationPublishingTestBase.cs
@@ -40,7 +40,7 @@ public abstract class AspireApplicationPublishingTestBase
             });
 
             // Publish does not use DCP - but it does validate this config object, so give it some sane values.
-            builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
+            builder.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
             {
                 [LearningTransportAnnotation.SettingsEnablePublish] = "true",
                 ["ASPIRE_DCP_PATH"] = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".aspire/bin/dcp"),
